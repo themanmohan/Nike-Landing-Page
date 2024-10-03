e7851574e3c72ab173f49381e94bf14a6d52ac41
themanmohan <gitmail223344@gmail.com>
2024-10-04 00:44:31 +0530

    refactor: adding review section

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -4,10 +4,14 @@ import Button from "./Button";
 import ShoeCard from "./ShoeCard‎";
 import Stats from "./StatsCounter";
 import PopularProductCard from "./PopularProductCard";
+import ServiceCard from "./ServiceCard";
+import ReviewCard from "./ReviewCard";
 export {
     Nav,
     Button,
     ShoeCard,
+    ReviewCard,
+    ServiceCard,
     Stats,
     PopularProductCard
 }
